cb89b5da7510f3c92e24152829f4ee6bc451aa0f
naseem <naseemkhan702167@gmail.com>
2022-01-20 21:32:04 +0530

    edit env file

--- include/db/db.php
+++ include/db/db.php
@@ -1,5 +1,6 @@
 <?php
-$conn = mysqli_connect(getenv("DB_SERVER"),"root", getenv("DB_PASS"), getenv("DB_NAME"));
+// $conn = mysqli_connect(getenv("DB_SERVER"),"root", getenv("DB_PASS"), getenv("DB_NAME"));
+$conn = mysqli_connect(getenv("DB_SERVER"),"zjbvmhxobesanr", getenv("DB_PASS"), getenv("DB_NAME"),5432);
 // $conn = mysqli_connect(getenv("DB_SERVER"), getenv("DB_USER"), '', getenv("DB_NAME"));
 if (mysqli_connect_errno()) {
      die ("Connection Fail" . mysqli_connect_error());
